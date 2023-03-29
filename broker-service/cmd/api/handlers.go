2813acee908ae2b79f2ef8068ed4241ea4b79227
lipandr <andrey.lipayev@gmail.com>
2023-03-29 12:39:49 +0300

    Adding a route and handler on the Broker to communicate with the logger service

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -10,12 +10,18 @@ import (
 
 type RequestPayload struct {
 	Action string      `json:"action"`
-	Auth   AuthPayload `json:"auth"`
+	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, _ *http.Request) {
@@ -36,6 +42,8 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
+	case "log":
+		app.logItem(w, requestPayload.Log)
 	default:
 		_ = app.errorJSON(w, errors.New("unknown action"), http.StatusBadRequest)
 	}
@@ -89,6 +97,40 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 		Message: "Authenticated",
 		Data:    jsonFromService.Data,
 	}
+
+	_ = app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	// create json we'll send to the log endpoint
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	logServiceURL := "http://log-service/log"
+	request, err := http.NewRequest(
+		http.MethodPost,
+		logServiceURL,
+		bytes.NewBuffer(jsonData),
+	)
+	if err != nil {
+		_ = app.errorJSON(w, err)
+		return
+	}
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		_ = app.errorJSON(w, err)
+		return
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(response.Body)
+	if response.StatusCode != http.StatusAccepted {
+		_ = app.errorJSON(w, errors.New("error calling log service"))
+		return
+	}
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "Log entry created successfully!"
 
 	_ = app.writeJSON(w, http.StatusAccepted, payload)
 }
